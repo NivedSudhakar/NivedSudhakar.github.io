f687908c83284a38c84a917085c7555e9f5cd97a
Nived Sudhakar <Nived.s.2018@gmail.com>
2023-12-22 16:07:28 -0800

    certifications removed

--- src/Components/EdCardsContainer.js
+++ src/Components/EdCardsContainer.js
@@ -1,83 +1,69 @@
-import { render } from '@testing-library/react';
-import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
-import CardItem3 from './EdCardItem'
-import "./EdCards.css"
+import { render } from "@testing-library/react";
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import CardItem3 from "./EdCardItem";
+import "./EdCards.css";
 
-
-
-
-
-export const EdCardsContainer = ({onClick}) => {
+export const EdCardsContainer = ({ onClick }) => {
   const [Card, setCard] = useState(0);
 
-const VCCardsText = ["AP Computer Science A - 5", "AP Statistics", "AP Physics 1", "AP Human Geography", "AP Chemistry"]
-const MCCardsText = ["Introduction to Data Structures with Java", "Robotics and Embedded Systems"]
-const CertCardsText = ["Harvardx - CS50x", "GTx - Linear Algebra I"]
+  const VCCardsText = [
+    "AP Computer Science A - 5",
+    "AP Statistics",
+    "AP Physics 1",
+    "AP Human Geography",
+    "AP Chemistry",
+  ];
+  const MCCardsText = [
+    "Introduction to Data Structures with Java",
+    "Robotics and Embedded Systems",
+  ];
+  const CertCardsText = ["Harvardx - CS50x", "GTx - Linear Algebra I"];
 
-
-
- 
   return (
-    <div className='cardsnstuffEd'>
+    <div className="cardsnstuffEd">
       <h1>Education</h1>
-      <div className='cardsEd'>
-        <div className='cards__containerEd'>
+      <div className="cardsEd">
+        <div className="cards__containerEd">
           <div className="cards__wrapperEd">
-            <ul className='cards__itemsEd'>
-
-
-              <div className='clicksection' onClick={() => {setCard(0)}}>
-                <CardItem3
-                  text1 = "4.75 Valley Christian High School GPA"
-                />
-              </div>
-              
-
-              <div className='clicksection' onClick={() => {setCard(1)}}>
-                <CardItem3
-                  text1 = "4.00 Mission College GPA"
-                />
+            <ul className="cards__itemsEd">
+              <div
+                className="clicksection"
+                onClick={() => {
+                  setCard(0);
+                }}
+              >
+                <CardItem3 text1="Valley Christian High School" />
               </div>
 
-              <div className='clicksection' onClick={() => {setCard(2)}}>
-                <CardItem3 
-                  text1 = "Certifications"
-                />
+              <div
+                className="clicksection"
+                onClick={() => {
+                  setCard(1);
+                }}
+              >
+                <CardItem3 text1="Mission College" />
               </div>
-              
-              
             </ul>
-            <ul className='cards__itemsEd'>
-              { Card === 0 ? <CardItem3
-                text1 = "APs taken in high school: "
-                details = {VCCardsText}
-                
-
-              /> : Card === 1 ? <CardItem3
-              text1 = "Mission College Courses: "
-              details = {MCCardsText}
-
-
-            /> :
-            <CardItem3
-              text1 = "Certifications: "
-              details = {CertCardsText}
-              
-
-            />
-            }
-              
+            <ul className="cards__itemsEd">
+              {Card === 0 ? (
+                <CardItem3
+                  text1="APs taken in high school: "
+                  details={VCCardsText}
+                />
+              ) : (
+                <CardItem3
+                  text1="Mission College Courses: "
+                  details={MCCardsText}
+                />
+              )}
             </ul>
           </div>
         </div>
+      </div>
     </div>
-    </div>
-    
-  )
-}
-
-
+  );
+};
 
 export default EdCardsContainer;
